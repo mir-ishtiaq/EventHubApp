5142d541e0531731131caba170687c891e6513ef
Mir Ishtiaq Uddin <108404327+mir-ishtiaq@users.noreply.github.com>
2023-08-22 05:21:15 +0600

    added include in urls

--- EventHubApp/urls.py
+++ EventHubApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     # path('admin/', admin.site.urls),
     path('events/', views.event_list, name='event_list'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
 ]

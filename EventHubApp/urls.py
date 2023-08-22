d413523e95f6f8ac1ce466bc66aa4842acdbb437
Mir Ishtiaq Uddin <108404327+mir-ishtiaq@users.noreply.github.com>
2023-08-22 11:03:50 +0600

    info, w/o reg,login

--- EventHubApp/urls.py
+++ EventHubApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
      # path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('events/', views.event_list, name='event_list'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
 ]
--- EventHubApp/views.py
+++ EventHubApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Event
 
+def home(request):
+    return render(request, 'base.html')
+
 def event_list(request):
     events = Event.objects.all()
     return render(request, 'event_list.html', {'events': events})
